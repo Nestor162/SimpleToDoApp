4bf199616af10f2b47fab47727468ee6f80d3e79
Nestor162 <nestorcicardini@gmail.com>
2023-10-04 16:01:50 +0200

    Add dialog layout

--- app/src/main/java/com/nestorcicardini/simpletodoapp/ToDoActivity.kt
+++ app/src/main/java/com/nestorcicardini/simpletodoapp/ToDoActivity.kt
@@ -1,9 +1,11 @@
 package com.nestorcicardini.simpletodoapp
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ToDoActivity : AppCompatActivity() {
 
@@ -11,7 +13,6 @@ class ToDoActivity : AppCompatActivity() {
         TaskCategory.Business,
         TaskCategory.Personal,
         TaskCategory.Other
-
     )
 
     private var tasks = mutableListOf(
@@ -24,17 +25,20 @@ class ToDoActivity : AppCompatActivity() {
     private lateinit var categoriesAdapter: CategoriesAdapter
     private lateinit var rvTasks: RecyclerView
     private lateinit var taskAdapter: TaskAdapter
+    private lateinit var fabAddTask: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_do)
         initComponent()
         initUI()
+        initListeners()
     }
 
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
 
     private fun initUI() {
@@ -48,4 +52,16 @@ class ToDoActivity : AppCompatActivity() {
             LinearLayoutManager(this)
         rvTasks.adapter = taskAdapter
     }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener { showDialog() }
+
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_new_task)
+        dialog.show()
+    }
+
 }
